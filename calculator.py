d839869896218fb60a5b9384a35123c8b6bed6c7
Park Hyean Sue <108973610+psue04@users.noreply.github.com>
2022-08-11 00:27:34 +0900

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#average number * 10/3
+
+
+number = input()
+
+def calculate_sm(number):
+    return (number + "*10/3 : ")
+    result = float(number) * 10 / 3
+    print (result)
+    
+calculate_sm(number)
