8b9b53e3e4ed28e39756a5a20fdf8d8dbd141a46
Park Hyean Sue <108973610+psue04@users.noreply.github.com>
2022-08-18 01:24:11 +0900

    Add files via upload

---
+++ Score grading.py
@@ -0,0 +1,14 @@
+#score grading
+
+score = int(input())
+
+if score >= 91:
+    print ("A")
+elif score >= 81:
+    print ("B")
+elif score >= 71:
+    print ("C")
+elif score >= 61:
+    print ("D")
+else:
+    print ("F")
